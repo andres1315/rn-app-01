1487991a3401b8afc155c27d70954e05977575aa
Andres Duque <andress1315@gmail.com>
2023-04-25 21:28:40 -0500

    feat: create component to appbar

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View } from 'react-native'
+import { StyledText } from './StyledText'
+import Constants from 'expo-constants'
+import { theme } from '../theme'
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.appBar.primary,
+    paddingTop: Constants.statusBarHeight + 10,
+    paddingBottom: 10,
+    paddingLeft: 10
+  },
+  text: {
+    color: theme.appBar.textPrimary
+  }
+})
+
+export const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <StyledText fontWeight='bold' style={styles.text}>
+        Repositories
+      </StyledText>
+
+    </View>
+  )
+}
