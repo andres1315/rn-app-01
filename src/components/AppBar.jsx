cf48a00ffb64ef4e8c3fd29d695414a3cdb2d9e7
Andres Duque <andress1315@gmail.com>
2023-04-25 23:01:18 -0500

    feat: add navbr with link and routes succesfully

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,26 +1,51 @@
-import { StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import { StyledText } from './StyledText'
 import Constants from 'expo-constants'
 import { theme } from '../theme'
+import { Link, useLocation } from 'react-router-native'
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.appBar.primary,
-    paddingTop: Constants.statusBarHeight + 10,
-    paddingBottom: 10,
-    paddingLeft: 10
+    flexDirection: 'row',
+    paddingTop: Constants.statusBarHeight + 10
   },
   text: {
+    color: theme.appBar.textSecondary,
+    paddingHorizontal: 10
+  },
+  scroll: {
+    paddingBottom: 15
+  },
+  active: {
     color: theme.appBar.textPrimary
   }
 })
 
+const AppBarTab = ({ children, to }) => {
+  const { pathname } = useLocation()
+  const active = pathname === to
+  const textStyles = [
+    styles.text,
+    active && styles.active
+  ]
+  return (
+    <Link to={to}>
+      <StyledText active fontWeight='bold' style={textStyles}>
+        {children}
+      </StyledText>
+    </Link>
+  )
+}
+
 export const AppBar = () => {
   return (
     <View style={styles.container}>
-      <StyledText fontWeight='bold' style={styles.text}>
-        Repositories
-      </StyledText>
+      <ScrollView horizontal style={styles.scroll}>
+        <AppBarTab to='/' style={styles.text}>Repositories</AppBarTab>
+        <AppBarTab to='/signin' style={styles.text}>Sing In</AppBarTab>
+
+      </ScrollView>
 
     </View>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,28 @@
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 import { RepositoryList } from './RepositoryList'
 import { AppBar } from './AppBar'
+import { Route, Routes } from 'react-router-native'
+
+const Signin = () => {
+  return (
+    <Text>Working on it</Text>
+  )
+}
+const notFound = () => {
+  return (
+    <Text>No exist</Text>
+  )
+}
+
 export const Main = () => {
   return (
     <View style={{ flex: 1 }}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path='/' exact Component={RepositoryList} />
+        <Route path='/signin' exact Component={Signin} />
+        <Route path='*' Component={notFound} />
+      </Routes>
     </View>
   )
 }
