a955d8a1227473b07ab84d6a55c2d7ffcccc98c9
Andres Duque <andress1315@gmail.com>
2023-04-25 23:00:45 -0500

    feat: add route

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { Main } from './src/components/Main'
+import { NativeRouter } from 'react-router-native'
 export default function App () {
-  return <Main />
+  return <NativeRouter><Main /></NativeRouter>
 }
